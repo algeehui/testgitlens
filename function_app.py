8c8c6dd5397d8d5e0164c11978c249eff8efb360
Alvin Hui <alvin.hui@welbehealth.com>
2024-11-06 09:25:42 -0800

    f_name and l_name

--- function_app.py
+++ function_app.py
@@ -10,7 +10,8 @@ app = func.FunctionApp()
 def HelloWorld(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
+    f_name = req.params.get('fname')
+    l_name = req.params.get('lname')
     if not name:
         try:
             req_body = req.get_json()
@@ -19,8 +20,8 @@ def HelloWorld(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if f_name and l_name:
+        return func.HttpResponse(f"Hello, {f_name} {l_name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              f"This HTTP triggered function executed successfully. My name is {os.getenv('MyName')}.",
